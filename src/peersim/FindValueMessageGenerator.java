0b5b43e26fa83849beae4c947dc94bd4d3942835
Nawras Nazar <Nawras Nazar@iris>
2019-02-22 11:05:53 +0900

    added locality to the random query selection.

--- src/peersim/FindValueMessageGenerator.java
+++ src/peersim/FindValueMessageGenerator.java
@@ -9,11 +9,10 @@ import peersim.edsim.EDSimulator;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Random;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
-import joinery.DataFrame;
 
 /**
  * This control generates random search traffic from nodes to random destination node.
@@ -37,9 +36,13 @@ public static int i=0;
 	 */
 	private final int pid;
 	
-	private static ArrayList<BigInteger> RepeatedKeys = new ArrayList<>();
+	private static ArrayList<BigInteger> KeysWithFrequency = new ArrayList<>();
+	private static Map<BigInteger, Boolean> halfhmap1 = new HashMap<>();
+	private static Map<BigInteger, Boolean> halfhmap2 = new HashMap<>();
+	private static ArrayList<BigInteger> halfFrequency1 = new ArrayList<>();
+	private static ArrayList<BigInteger> halfFrequency2 = new ArrayList<>();
 	private static boolean keyListContructionDone = false;
-
+	public static Map<BigInteger, Integer> issuedQuery = new HashMap<>();
 	// ______________________________________________________________________________________________
 	public FindValueMessageGenerator(String prefix) {
 		pid = Configuration.getPid(prefix + "." + PAR_PROT);
@@ -84,31 +87,83 @@ public static int i=0;
 		return null;
 	}
 	
-	
+	/**
+	 * generating queries according to the frequency of the keyword in the original dataset
+	 * @return
+	 * 		the randomly chosen message
+	 */
 	private Message generateFindValueMessageTrueProbability(){
-		
+		ArrayList<BigInteger> tmp = null;
 		if (!keyListContructionDone) {
 			// generate a list of truly stored keys.
-			for(BigInteger keyStatus: StoreMessageGenerator.generatedKeyList_status.keySet()) {
+			/*for(BigInteger keyStatus: StoreMessageGenerator.generatedKeyList_status.keySet()) {
 				if(StoreMessageGenerator.generatedKeyList_status.get(keyStatus)) {
 					int frequency = StoreMessageGenerator.generatedKeyList_weight.getOrDefault(keyStatus, -1);
 					for(int i=0; i<frequency; i++) {
-						RepeatedKeys.add(keyStatus);
+						KeysWithFrequency.add(keyStatus);
+					}
+				}
+			}*/
+			keyListContructionDone = true;
+			// System.out.print(keys.size());
+			
+			// locality
+		    int count=0;
+		    
+		    // creating two halves of keyword set
+		    for(Map.Entry<BigInteger, Boolean> entry : StoreMessageGenerator.generatedKeyList_status.entrySet()) {
+		        (count<(StoreMessageGenerator.generatedKeyList_status.size()/3) ? halfhmap1:halfhmap2).put(entry.getKey(), entry.getValue());
+		        count++;
+		    }
+		    
+		    // adding frequency to each group
+		    // group 1
+		    for(BigInteger keyStatus: halfhmap1.keySet()) {
+				if(halfhmap1.get(keyStatus)) {
+					int frequency = StoreMessageGenerator.generatedKeyList_weight.getOrDefault(keyStatus, -1);
+					for(int i=0; i<frequency; i++) {
+						halfFrequency1.add(keyStatus);
 					}
 				}
 			}
-			keyListContructionDone = true;
-			// System.out.print(keys.size());
+		    
+		    // group 2
+		    for(BigInteger keyStatus: halfhmap2.keySet()) {
+				if(halfhmap2.get(keyStatus)) {
+					int frequency = StoreMessageGenerator.generatedKeyList_weight.getOrDefault(keyStatus, -1);
+					for(int i=0; i<frequency; i++) {
+						halfFrequency2.add(keyStatus);
+					}
+				}
+			}
+		    
 		}
 		
+		int randGroup = ThreadLocalRandom.current().nextInt(0, 10);
+	    
+	    if(randGroup < 9 ) {
+	    	tmp = halfFrequency1;
+	    }
+	    else {
+	    	tmp = halfFrequency2;
+	    }
+		// randomly choosing a message
 		int rand = 0;
 		try {
-			rand = CommonState.r.nextInt(RepeatedKeys.size());
+			rand = CommonState.r.nextInt(tmp.size());
 		} catch(IllegalArgumentException ex) { // IllegalArgumentException happens when generatedKeyList_weight.size = 0
 			ex.printStackTrace();
 			return null;
 		}
-		BigInteger key = RepeatedKeys.get(rand);
+		
+		BigInteger key = tmp.get(rand);
+		if(issuedQuery.containsKey(key)) {
+			//++KademliaObserver.h;
+			issuedQuery.put(key, issuedQuery.get(key)+1);
+		}else {
+			issuedQuery.put(key, 0);
+		}
+		
 		Message m = Message.makeFindValue(key);
 		//System.out.print(m.body+" rand: " + rand+" - ");
 		m.timestamp = CommonState.getTime();
@@ -128,7 +183,7 @@ public static int i=0;
 		Node start;
 		do {
 			int rand = CommonState.r.nextInt(Network.size());
-			rand = ThreadLocalRandom.current().nextInt(0, Network.size());
+			// rand = ThreadLocalRandom.current().nextInt(0, Network.size());
 			// System.out.println("the node ind " + rand);
 			start = Network.get(rand);
 			// the node 162 always failed to find msg 679695804144180158154957817433688509811568326000
